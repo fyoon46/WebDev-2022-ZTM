4812a4e64fe16832b90a75e280c0692be6c249c2
Yusang Francis Yoon <fyoon46@gmail.com>
2022-11-04 23:25:39 +0900

    Javascript Exercise

--- Section11_Javascript/Exercises/Exercise7/exercise7-SOLUTIONS.js
+++ Section11_Javascript/Exercises/Exercise7/exercise7-SOLUTIONS.js
@@ -1,12 +1,12 @@
 // Create an object and an array which we will use in our facebook exercise. 
 
 // 1. Create an object that has properties "username" and "password". Fill those values in with strings.
-{
+var user = {
 	username: "andrei",
 	password: "supersecret"
 }
 
-// 2. Create an array which contains the object you have made above and name the arry "database"
+// 2. Create an array which contains the object you have made above and name the array "database"
 var database = [
 	{
 		username: "andrei",
