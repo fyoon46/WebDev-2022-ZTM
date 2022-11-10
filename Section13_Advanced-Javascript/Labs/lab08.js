6f8b08f775f75d86a8b140bfcd8b7d91ca0b3d87
Yusang Francis Yoon <fyoon46@gmail.com>
2022-11-10 12:13:24 +0900

    Advanced Javascript : Labs

---
+++ Section13_Advanced-Javascript/Labs/lab08.js
@@ -0,0 +1,26 @@
+const first = () => {
+    const greet = "Hi";
+    return () => {
+        console.log(greet);
+    };
+}
+
+const newFunc = first();
+newFunc();
+
+// Closures - A function ran, a function executed. It's never going to execute again.
+// BUT it's going to remember that there are references to those variables,
+// so the child scope always has access to the parent scope.
+
+// Currying
+const multiply = (a, b) => a * b;
+const curriedMultiply = (a) => (b) => a * b;
+console.log(curriedMultiply(3)(4));
+
+const multiplyBy5 = curriedMultiply(5);
+console.log(multiplyBy5(11));
+
+// Compose
+const compose = (f, g) => (a) => f(g(a));
+const sum = (num) => num + 1;
+console.log(compose(sum, sum)(5));
